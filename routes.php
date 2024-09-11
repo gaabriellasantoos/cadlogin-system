dee187f74df142383537b6c6af3a96d7481fd6c9
Gabriella Santos <goncalves.gabriella@alunoescolassociais.org.br>
2024-09-11 08:58:28 -0300

    icluindo arquivos de controlador

---
+++ routes.php
@@ -0,0 +1,9 @@
+<?php
+
+//Inclui arquivos de controlador necessários para lidar com diferentes ações
+require 'controllers/AuthController.php'; //Inclui o controlador de autenticação
+require 'controllers/UserController.php'; //Inclui o controlador de usários
+require 'controllers/DashboardController.php'; //Inclui o controlador de dashboard
+
+
+?>
