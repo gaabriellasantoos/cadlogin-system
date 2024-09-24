9820e99c8379bd27cae1dac17471d3c55216e798
Gabriella Santos <goncalves.gabriella@alunoescolassociais.org.br>
2024-09-24 08:00:00 -0300

    função para encontrar um usuario pelo email, obter conexão com o BD, retorno de dados do usuario encontrado como um array, cria função que localiza usuario

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,7 +1,7 @@
 <?php
 //Requer o arquivo user que contém o model user com as funções de manipulação de dados de usuários
 
-require_once 'model/user.php';
+require_once 'models/user.php';
 
 class AuthController
 {
@@ -12,19 +12,4 @@ class AuthController
   }
 }
 
-?>
-
-<?php
-//requer o arquivo user que contém o model user com as fubções de manipulaão de dados de usuários
-require_once 'models/user.php';
- 
-class AuthController
-{
-    //função responsável responsável para realizar login
-    public function login()
-    {
-        //Verifica se a requisição HTTP é do tipo POST, ou seja, se o formulário foi enviado
-       
-    }
-}
 ?>
